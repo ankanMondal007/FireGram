f862df1b946d2ddc2f52b40477dc96a091ee59cd
ankanMondal007 <mailtoankan2001@gmail.comn>
2022-07-23 10:35:05 +0530

    build folder added

---
+++ build/precache-manifest.261d0cde26a4828da7134a3951ba43d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97b65c88d9fa09996843e2830c45cf5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0cf7b9b18231a4f203e",
+    "url": "/static/css/main.e3bd3a6e.chunk.css"
+  },
+  {
+    "revision": "f1f26490ac12fa552146",
+    "url": "/static/js/2.b18b1db7.chunk.js"
+  },
+  {
+    "revision": "9264dff9dd80d6c45a41d6a59744ac9a",
+    "url": "/static/js/2.b18b1db7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0cf7b9b18231a4f203e",
+    "url": "/static/js/main.aab27ca3.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.aab27ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84af4f01c0eb442c8e6f",
+    "url": "/static/js/runtime-main.c38bc1b2.js"
+  }
+]);
